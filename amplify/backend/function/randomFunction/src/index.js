196991ac145ccd8f8a466c9b737c84c2eeb416f3
Roshane Pascual <rotp@amazon.com>
2025-08-21 15:32:12 -0700

    uncommented lines in randomFunction index.js

--- amplify/backend/function/randomFunction/src/index.js
+++ amplify/backend/function/randomFunction/src/index.js
@@ -7,11 +7,10 @@ exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
         body: JSON.stringify('Hello from Lambda!'),
     };
 };
